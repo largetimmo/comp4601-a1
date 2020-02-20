190d5c02a126fc9ed9b94157d0362c1e19add54c
wen.D <35638246+suohongliu@users.noreply.github.com>
2020-02-19 19:09:22 -0500

    get id (not complete)

--- src/controller/SDAController.java
+++ src/controller/SDAController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import dao.Document;
+
+import javax.print.Doc;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
 
 @Path("/sda")
 public class SDAController {
@@ -9,5 +16,12 @@ public class SDAController {
     @GET
     public String whoAmI(){
         return "COMP4601 Searchable Document Archive: Junhao Chen And Mr. Liu";
+    }
+
+    @GET
+    @Path("id")
+    @Produces(MediaType.APPLICATION_XML)
+    public Document getDocumentByIdXML(@PathParam("id") String id){
+
     }
 }
